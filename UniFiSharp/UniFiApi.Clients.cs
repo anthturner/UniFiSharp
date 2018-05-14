31d48397884b559b1922ec7c681a368ea940eb8e
Dan Inden <dan.inden@outlook.com>
2018-05-14 07:20:29 +0200

    block/unblock and set/unset usergroup

--- UniFiSharp/UniFiApi.Clients.cs
+++ UniFiSharp/UniFiApi.Clients.cs
@@ -24,5 +24,39 @@ namespace UniFiSharp
                 mac = macAddress
             });
         }
+
+        public async Task ClientBlock(string macAddress)
+        {
+            await RestClient.UniFiPost($"api/s/{Site}/cmd/stamgr", new
+            {
+                cmd = "block-sta",
+                mac = macAddress
+            });
+        }
+
+        public async Task ClientUnblock(string macAddress)
+        {
+            await RestClient.UniFiPost($"api/s/{Site}/cmd/stamgr", new
+            {
+                cmd = "unblock-sta",
+                mac = macAddress
+            });
+        }
+
+        public async Task ClientSetUsergroup(JsonClient client, JsonUserGroup userGroup)
+        {
+            await RestClient.UniFiPut($"api/s/{Site}/rest/user/{client._id}", new
+            {
+                usergroup_id = userGroup?._id ?? string.Empty
+            });
+        }
+
+        public async Task ClientUnsetUsergroup(JsonClient client)
+        {
+            await RestClient.UniFiPut($"api/s/{Site}/rest/user/{client._id}", new
+            {
+                usergroup_id = string.Empty
+            });
+        }
     }
 }
