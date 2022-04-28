6330ceee6dec305c6514ef4133f29094d0084e44
Anthony Turner <225599+anthturner@users.noreply.github.com>
2022-04-28 14:52:52 -0400

    start of extended json object docs

---
+++ UniFiSharp/DataTypeConversionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UniFiSharp
+{
+    public static class DataTypeConversionExtensions
+    {
+        static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        public static string WithSizeSuffix(this double value, int decimalPlaces = 1) => WithSizeSuffix((long)value, decimalPlaces);
+        public static string WithSizeSuffix(this float value, int decimalPlaces = 1) => WithSizeSuffix((long)value, decimalPlaces);
+        public static string WithSizeSuffix(this int value, int decimalPlaces = 1) => WithSizeSuffix((long)value, decimalPlaces);
+        public static string WithSizeSuffix(this long value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + WithSizeSuffix(-value, decimalPlaces); }
+
+            int i = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[i]);
+        }
+    }
+}
--- UniFiSharp/Json/JsonBroadcastDevice.cs
+++ UniFiSharp/Json/JsonBroadcastDevice.cs
@@ -1,15 +1,31 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace UniFiSharp.Json
 {
     public class JsonBroadcastDevice
     {
+        /// <summary>
+        /// If the Broadcast Device is connected
+        /// </summary>
+        [DisplayName("Connected?")]
+        [IncludedInVisualization(VisualizationModes.Both)]
         [JsonProperty("connected")]
         public bool connected { get; set; }
 
+        /// <summary>
+        /// Broadcast Device MAC address
+        /// </summary>
+        [DisplayName("MAC Address")]
+        [IncludedInVisualization(VisualizationModes.Both)]
         [JsonProperty("mac")]
         public string mac { get; set; }
 
+        /// <summary>
+        /// Broadcast Device volume level
+        /// </summary>
+        [DisplayName("Volume")]
+        [IncludedInVisualization]
         [JsonProperty("volume")]
         public long volume { get; set; }
     }
